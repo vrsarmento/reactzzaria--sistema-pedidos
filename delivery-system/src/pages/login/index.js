57c41e001952a71e2b9bc054ed0999b110f36e27
Victor Rocha <vrsarmento@gmail.com>
2021-01-12 00:55:21 -0300

    Firebase install and config

--- delivery-system/src/pages/login/index.js
+++ delivery-system/src/pages/login/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+import firebase from 'firebase/app'
 import { Button, Grid } from '@material-ui/core'
 import { ReactComponent as LogoSvg } from './logo-react-zzaria.svg'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyASgB8RSo95maM5uhLW_5pYKqXa1egNdlg',
+  authDomain: 'reactzzaria-18f90.firebaseapp.com',
+  projectId: 'reactzzaria-18f90',
+  storageBucket: 'reactzzaria-18f90.appspot.com',
+  messagingSenderId: '737845885147',
+  appId: '1:737845885147:web:a1411b0ce007a7b80fb4fe'
+}
+firebase.initializeApp(firebaseConfig)
 
 const Login = () => (
   <Container>
