1c3632e3934d08c832ac1e34130d76c35c35948e
Victor Rocha <vrsarmento@gmail.com>
2021-01-11 21:06:06 -0300

    Material-UI config || Styled-Components installation

--- delivery-system/src/app.js
+++ delivery-system/src/app.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { CssBaseline } from '@material-ui/core'
 
 import MainPage from 'pages/main'
 import Login from 'pages/login'
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path='/login' component={Login} />
-      <Route path='/' exact component={MainPage} />
-    </Switch>
-  </BrowserRouter>
+  <>
+    <CssBaseline />
+    <BrowserRouter>
+      <Switch>
+        <Route path='/login' component={Login} />
+        <Route path='/' exact component={MainPage} />
+      </Switch>
+    </BrowserRouter>
+  </>
 )
 
 export default App
