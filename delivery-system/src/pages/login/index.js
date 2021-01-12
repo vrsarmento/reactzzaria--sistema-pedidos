47c25bfa25623f753a7134b01723cf299194a2bf
Victor Rocha <vrsarmento@gmail.com>
2021-01-11 22:15:24 -0300

    Login Page initial styles

--- delivery-system/src/pages/login/index.js
+++ delivery-system/src/pages/login/index.js
@@ -1,7 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Button, Grid } from '@material-ui/core'
+import { ReactComponent as LogoSvg } from './logo-react-zzaria.svg'
 
 const Login = () => (
-  <h1>Login</h1>
+  <Container>
+    <Grid container justify='center' spacing={5}>
+      <Grid item xs={12}>
+        <Logo />
+      </Grid>
+
+      <Grid item xs={12} container justify='center'>
+        <GitHubButton>
+          Entrar com GitHub
+        </GitHubButton>
+      </Grid>
+    </Grid>
+  </Container>
 )
+
+const Container = styled.div`
+  padding: 20px;
+`
+
+const Logo = styled(LogoSvg)`
+  width: 100%;
+`
+
+const GitHubButton = styled(Button).attrs({
+  variant: 'contained',
+  fullWidth: true
+})`
+  && {
+    font-size: 25px;
+    max-width: 480px;
+    padding: 15px;
+    text-transform: none;
+  }
+`
 
 export default Login
