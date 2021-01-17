403510e32a5e43fc7fcde660d05976fefa056a93
Victor Rocha <vrsarmento@gmail.com>
2021-01-17 02:15:08 -0300

    app.js, auth.js and login.js functions scope strategy adjustments

--- delivery-system/src/pages/login/index.js
+++ delivery-system/src/pages/login/index.js
@@ -1,39 +1,11 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
-import firebase from 'services/firebase'
+import { AuthContext } from 'contexts/auth'
 import { Button, Grid } from '@material-ui/core'
 import { ReactComponent as LogoSvg } from './logo-react-zzaria.svg'
 
 function Login () {
-  const [userInfo, setUserInfo] = useState({
-    isUserLoggedIn: false,
-    user: null
-  })
-
-  const { user, isUserLoggedIn } = userInfo
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      setUserInfo({
-        isUserLoggedIn: !!user,
-        user
-      })
-    })
-  }, [])
-
-  const handleLogin = useCallback(() => {
-    const provider = new firebase.auth.GithubAuthProvider()
-    firebase.auth().signInWithRedirect(provider)
-  }, [])
-
-  const handleLogout = useCallback(() => {
-    firebase.auth().signOut().then(() => {
-      setUserInfo({
-        isUserLoggedIn: false,
-        user: null
-      })
-    })
-  }, [])
+  const { login } = useContext(AuthContext)
 
   return (
     <Container>
@@ -43,22 +15,9 @@ function Login () {
         </Grid>
 
         <Grid item xs={12} container justify='center'>
-          {isUserLoggedIn && (
-            <>
-              <pre>{user.displayName}</pre>
-              <Button variant='contained' onClick={handleLogout}>
-                Sair
-              </Button>
-            </>
-          )}
-
-          {!isUserLoggedIn && (
-            <>
-              <GitHubButton onClick={handleLogin}>
-                Entrar com GitHub
-              </GitHubButton>
-            </>
-          )}
+          <GitHubButton onClick={login}>
+            Entrar com GitHub
+          </GitHubButton>
         </Grid>
       </Grid>
     </Container>
