c0936a0fad58069aedd8f3f49bb8a50b90743386
Victor Rocha <vrsarmento@gmail.com>
2021-01-27 01:03:46 -0300

    Structuring Internal Pages || Isolating fake-data

---
+++ delivery-system/src/pages/choose-pizza-size/index.js
@@ -0,0 +1,121 @@
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import { AuthContext } from 'contexts/auth'
+import {
+  Divider as MaterialDivider,
+  Grid,
+  Paper,
+  Typography
+} from '@material-ui/core'
+import pizzasSizes from 'fake-data/pizzas-sizes'
+
+const ChoosePizzaSize = () => {
+  const { userInfo } = useContext(AuthContext)
+
+  return (
+    <>
+      <Grid container direction='column' alignItems='center'>
+        <Title variant='h4'>
+          O que vai ser hoje, {userInfo.user.firstName}?
+        </Title>
+        <Title variant='h5'>
+          Escolha o tamanho da pizza:
+        </Title>
+      </Grid>
+
+      <PizzasGrid>
+        {pizzasSizes.map((pizza) => (
+          <Grid item key={pizza.id} xs>
+            <PaperPizza>
+              <Pizza>
+                <PizzaText>{pizza.size}cm</PizzaText>
+              </Pizza>
+
+              <Divider />
+
+              <Typography variant='h5'>{pizza.name}</Typography>
+              <Typography>
+                {pizza.slices} fatias, {' '}
+                {pizza.flavours} {' '}
+                {singularOrPlural(pizza.flavours, 'sabor', 'sabores')}
+              </Typography>
+            </PaperPizza>
+          </Grid>
+        ))}
+      </PizzasGrid>
+    </>
+  )
+}
+
+function singularOrPlural (amount, singular, plural) {
+  return amount === 1 ? singular : plural
+}
+
+const Title = styled(Typography).attrs({
+  gutterBottom: true,
+  align: 'center'
+})``
+
+const Divider = styled(MaterialDivider)`
+  width: 100%;
+  margin: 20px 0;
+`
+
+const PizzasGrid = styled(Grid).attrs({
+  container: true,
+  spacing: 16
+})`
+  padding: 20px;
+`
+
+const PaperPizza = styled(Paper)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-width: 250px;
+  padding: 20px 0;
+`
+
+const Pizza = styled.div`
+  width: 200px;
+  height: 200px;
+  border: 1px solid #ccc;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &::before,
+  &::after {
+    content: '';
+    background: #ccc;
+    position: absolute;
+    transform: rotate(45deg);
+  }
+
+  &::before {
+    width: 180px;
+    height: 1px;
+  }
+
+  &::after {
+    width: 1px;
+    height: 180px;
+  }
+`
+
+const PizzaText = styled(Typography).attrs({
+  variant: 'h5'
+})`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  width: 80px;
+  background: #fff;
+  border-radius: 50%;
+  position: relative;
+  z-index: 1;
+`
+
+export default ChoosePizzaSize
--- delivery-system/src/pages/main/index.js
+++ delivery-system/src/pages/main/index.js
@@ -1,96 +1,30 @@
-import React, { useContext } from 'react'
+import React, { Suspense } from 'react'
+import { Switch, Route } from 'react-router-dom'
 import styled from 'styled-components'
-import { AuthContext } from 'contexts/auth'
-import {
-  Divider as MaterialDivider,
-  Grid,
-  Paper,
-  Typography,
-  withStyles
-} from '@material-ui/core'
+import { withStyles } from '@material-ui/core'
 import Header from './header'
 
-const Main = () => {
-  const { userInfo } = useContext(AuthContext)
+const ChoosePizzaSize = React.lazy(() => import('pages/choose-pizza-size'))
 
-  return (
-    <>
-      <Header />
+const Main = () => (
+  <>
+    <Header />
 
-      <Spacer />
+    <Spacer />
 
-      <Content>
-        <Grid container direction='column' alignItems='center'>
-          <Title variant='h4'>
-            O que vai ser hoje, {userInfo.user.firstName}?
-          </Title>
-          <Title variant='h5'>
-            Escolha o tamanho da pizza:
-          </Title>
-        </Grid>
-
-        <PizzasGrid>
-          {pizzaSizes.map((pizza) => (
-            <Grid item key={pizza.id} xs>
-              <PaperPizza>
-                <Pizza>
-                  <PizzaText>{pizza.size}cm</PizzaText>
-                </Pizza>
-
-                <Divider />
-
-                <Typography variant='h5'>{pizza.name}</Typography>
-                <Typography>
-                  {pizza.slices} fatias, {' '}
-                  {pizza.flavours} {' '}
-                  {singularOrPlural(pizza.flavours, 'sabor', 'sabores')}
-                </Typography>
-              </PaperPizza>
-            </Grid>
-          ))}
-        </PizzasGrid>
-      </Content>
-
-    </>
-  )
-}
-
-function singularOrPlural (amount, singular, plural) {
-  return amount === 1 ? singular : plural
-}
-
-const pizzaSizes = [
-  {
-    id: 0,
-    name: 'Pequena',
-    size: 28,
-    slices: 2,
-    flavours: 1
-  },
-  {
-    id: 1,
-    name: 'Média',
-    size: 30,
-    slices: 6,
-    flavours: 2
-  },
-  {
-    id: 2,
-    name: 'Grande',
-    size: 32,
-    slices: 8,
-    flavours: 3
-  }
-]
+    <Content>
+      <Suspense fallback='Loading...'>
+        <Switch>
+          <Route path='/' exact component={ChoosePizzaSize} />
+        </Switch>
+      </Suspense>
+    </Content>
+  </>
+)
 
 const Content = styled.main`
   padding: 20px;
 `
-
-const Title = styled(Typography).attrs({
-  gutterBottom: true,
-  align: 'center'
-})``
 
 const style = (theme) => ({
   main: theme.mixins.toolbar
@@ -99,67 +33,5 @@ const style = (theme) => ({
 const Spacer = withStyles(style)(({ classes }) => (
   <div className={classes.main} />
 ))
-
-const Divider = styled(MaterialDivider)`
-  width: 100%;
-  margin: 20px 0;
-`
-
-const PizzasGrid = styled(Grid).attrs({
-  container: true,
-  spacing: 16
-})`
-  padding: 20px;
-`
-
-const PaperPizza = styled(Paper)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-width: 250px;
-  padding: 20px 0;
-`
-
-const Pizza = styled.div`
-  width: 200px;
-  height: 200px;
-  border: 1px solid #ccc;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  &::before,
-  &::after {
-    content: '';
-    background: #ccc;
-    position: absolute;
-    transform: rotate(45deg);
-  }
-
-  &::before {
-    width: 180px;
-    height: 1px;
-  }
-
-  &::after {
-    width: 1px;
-    height: 180px;
-  }
-`
-
-const PizzaText = styled(Typography).attrs({
-  variant: 'h5'
-})`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 80px;
-  width: 80px;
-  background: #fff;
-  border-radius: 50%;
-  position: relative;
-  z-index: 1;
-`
 
 export default Main
