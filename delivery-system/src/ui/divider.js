d801d85ec5a0538d4f9693c8d4a3b9f84337bfbb
Victor Rocha <vrsarmento@gmail.com>
2021-01-30 02:00:32 -0300

    Divider UI

---
+++ delivery-system/src/ui/divider.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { Divider as MaterialDivider } from '@material-ui/core'
+
+const Divider = styled(MaterialDivider)`
+  width: 100%;
+  margin: 20px 0;
+`
+
+export default Divider
--- delivery-system/src/ui/index.js
+++ delivery-system/src/ui/index.js
@@ -1,3 +1,4 @@
-export { default as Title, H3, H4, H5 } from './title'
+export { default as Divider } from './divider'
 export { default as HeaderContent } from './header-content'
 export { default as PizzasGrid } from './pizzas-grid'
+export { default as Title, H3, H4, H5 } from './title'
