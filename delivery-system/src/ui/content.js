70a71e731cd00905a7ef1bab739012681ac43cca
Victor Rocha <vrsarmento@gmail.com>
2021-03-12 00:40:43 -0300

    Content UI responsiveness

--- delivery-system/src/ui/content.js
+++ delivery-system/src/ui/content.js
@@ -1,8 +1,23 @@
+import React from 'react'
+import t from 'prop-types'
 import styled from 'styled-components'
+import { Container } from '@material-ui/core'
 
-const Content = styled.main`
+const Content = ({ children, ...props }) => (
+  <Main {...props}>
+    <Container>
+      {children}
+    </Container>
+  </Main>
+)
+
+const Main = styled.main`
   padding: ${({ theme }) => theme.spacing(3)}px;
   flex-grow: 1;
 `
+
+Content.propTypes = {
+  children: t.node.isRequired
+}
 
 export default Content
