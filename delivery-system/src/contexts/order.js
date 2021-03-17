6881635726521c47cbd2bf373267ecdb813aaa52
Victor Rocha <vrsarmento@gmail.com>
2021-03-17 18:49:33 -0300

    Fix OrderContext to clear the list of pizzas in the order

--- delivery-system/src/contexts/order.js
+++ delivery-system/src/contexts/order.js
@@ -4,10 +4,19 @@ import t from 'prop-types'
 const OrderContext = createContext()
 
 function OrderProvider ({ children }) {
-  const [pizzas, setPizzas] = useState([])
+  const [pizzas, addPizza] = useState([])
+  const [orderInProgress, setOrderInProgress] = useState(false)
 
   function addPizzaToOrder (pizza) {
-    setPizzas((pizzas) => pizzas.concat(pizza))
+    if (orderInProgress) {
+      return addPizza((pizzas) => pizzas.concat(pizza))
+    }
+    setOrderInProgress(true)
+    addPizza([pizza])
+  }
+
+  function sendOrder () {
+    setOrderInProgress(false)
   }
 
   return (
@@ -15,7 +24,8 @@ function OrderProvider ({ children }) {
       order: {
         pizzas
       },
-      addPizzaToOrder
+      addPizzaToOrder,
+      sendOrder
     }}
     >
       {children}
